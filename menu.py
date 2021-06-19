bed37d07da67738631f32c3f971da17b0a80c4f2
Animegod8 <60558787+Animegod8@users.noreply.github.com>
2021-06-19 16:34:52 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,6 +2,7 @@ print("welcome to textventure will you play")
 awnser = input()
 if awnser == "yes":
   print("test")
-  
- elif awmser == "no":
-  print("ok boomer")
+elif awnser == "no":
+    print("ok boomer")
+
+    
