1c46b46818ccbe05f7efb5d64c3a898eddc0401c
Animegod8 <60558787+Animegod8@users.noreply.github.com>
2021-06-22 17:02:35 -0600

    Add files via upload

---
+++ chose1.py
@@ -0,0 +1,16 @@
+def chocieOne():
+  print("you start your adventure at a tavern what do you do 1, lessz go inside, 2,nopenonononononono im out ")
+  awnser = input()
+  if awnser == "1":
+  print("you go in")
+elif awnser == "2":
+  print("nonononononononononono")
+  def pickPath():
+    print("whitch path do you take 1 fight a dragon, 2 go on an amazing conquest, 3 im here for some chips")
+    awnser2 = input()
+    if awnser2 == "1":
+      print("time to fight a frikin dragon")
+  elif awnser2 == "2":
+    print("time for epic counquest")
+  elif awnser2 == "3":
+    print("allright here is your frikin chipps get ou... mnomonmonomonmonomonmonomonmojnomnomonmonmon")
